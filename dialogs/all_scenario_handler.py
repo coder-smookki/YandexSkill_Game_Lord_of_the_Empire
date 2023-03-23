e422f84d74a33f324f3cbc10411d901b54b858b9
frxchii <fr3chi@gmail.com>
2023-03-23 21:22:02 +0500

    adding an opening scenario

--- dialogs/all_scenario_handler.py
+++
@@ -1,4 +0,0 @@
-from core.responseHelper import createCard
-
-def getResponse(event):
-    return createCard(event, 'Ты - истинный владыка?', 'Ты - истинный владыка?', None, None, buttons=['Да', 'Нет', 'Повторить ещё раз', 'Что ты умеешь?', 'Помощь', 'Выйти'])
