bf1ac78c0e516027b1c55db04dfac1dd2a04832d
elogrus <elogrus@gmail.com>
2023-03-25 20:42:50 +0500

    my PC => server

--- index.py
+++ index.py
@@ -1,23 +1,17 @@
-# from dialogs.Opening.Opening import *
-# from dialogs.Cashed import *
+from dialogs.Opening.Opening import *
+from dialogs.Cashed import *
 from core.skillAliceEmulator import *
-# from core.historyHandler import *
-# from core.yandexskillcore import startServer
-# from core.responseHelper import createCard
-# from handler import handler
-# import sys
-# from dotenv import load_dotenv
-# import os
-# load_dotenv()
+from core.historyHandler import *
+from core.yandexskillcore import startServer
+from core.responseHelper import createCard
+from handler import handler
+import sys
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-# sys.setrecursionlimit(5000)
+sys.setrecursionlimit(5000)
 
-# # Формат: "text // trueBtn // falseBtn // church army nation coffers // cardId"
-# # Пример: "text // asd // zxc // 0 0 -10 5 // zxc123"
-# history = """
-# "text // true // false // 0 0 0 20 // card"
-# "text // None // None // 0 0 0 0 // card"
-# """
 
 # концовки при переполнении/недостатка какой-то статы
 # формат такой-же
@@ -38,15 +32,17 @@ false:
     "full? // true // None // 100 100 100 100 // card"
 """
 
-skillEmulate(history,statsEnds)
 
-# host = os.environ.get('HOST')
-# if host:
-#     print(colored("+", "green"),'HOST найден:', host)
-#     startServer(host=host, handler=handler)
-# else:
-#     print(colored("-", "red"), 'HOST не найден! Запускаю сервер на 127.0.0.1')
-#     startServer(handler=handler)
+# skillEmulate(history,statsEnds)
+
+
+host = os.environ.get('HOST')
+if host:
+    print(colored("+", "green"),'HOST найден:', host)
+    startServer(host=host, handler=handler)
+else:
+    print(colored("-", "red"), 'HOST не найден! Запускаю сервер на 127.0.0.1')
+    startServer(handler=handler)
 
 
 
