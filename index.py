11daa30c6b42c5538dccc03044865a64d84b6e07
Кирилл <medved3loy@yandex.ru>
2023-03-26 00:19:52 +0500

    added the initial structure of adding statistics to each block

--- index.py
+++ index.py
@@ -8,20 +8,23 @@ from handler import handler
 import sys
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 sys.setrecursionlimit(5000)
-
 
 # концовки при переполнении/недостатка какой-то статы
 # формат такой-же
 # full - когда стата заполняется на 100+
 # empty - когда стата опускается до 0-
 statsEnds = {
-    "church": {"full": "\"fullChurch // None // None // // card\"", "empty": "\"emptyChurch // None // None // // card\""},
+    "church": {"full": "\"fullChurch // None // None // // card\"",
+               "empty": "\"emptyChurch // None // None // // card\""},
     "army": {"full": "\"fullArmy // None // None // // card\"", "empty": "\"emptyArmy // None // None // // card\""},
-    "nation": {"full": "\"fullNation // None // None // // card\"", "empty": "\"emptyNation // None // None // // card\""},
-    "coffers": {"full": "\"fullCoffers // None // None // // card\"", "empty": "\"emptyCoffers // None // None // // card\""},
+    "nation": {"full": "\"fullNation // None // None // // card\"",
+               "empty": "\"emptyNation // None // None // // card\""},
+    "coffers": {"full": "\"fullCoffers // None // None // // card\"",
+                "empty": "\"emptyCoffers // None // None // // card\""},
 }
 
 history = """
@@ -32,17 +35,12 @@ false:
     "full? // true // None // 100 100 100 100 // card"
 """
 
+skillEmulate(Opening, statsEnds, Cashed)
 
-# skillEmulate(history,statsEnds)
-
-
-host = os.environ.get('HOST')
-if host:
-    print(colored("+", "green"),'HOST найден:', host)
-    startServer(Opening, statsEnds, Cashed, host=host, handler=handler)
-else:
-    print(colored("-", "red"), 'HOST не найден! Запускаю сервер на 127.0.0.1')
-    startServer(Opening, statsEnds, Cashed, handler=handler)
-
-
-
+# host = os.environ.get('HOST')
+# if host:
+#     print(colored("+", "green"),'HOST найден:', host)
+#     startServer(Opening, statsEnds, Cashed, host=host, handler=handler)
+# else:
+#     print(colored("-", "red"), 'HOST не найден! Запускаю сервер на 127.0.0.1')
+#     startServer(Opening, statsEnds, Cashed, handler=handler)
