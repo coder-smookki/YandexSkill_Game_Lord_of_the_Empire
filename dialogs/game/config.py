fc33dfe495d1b841665573c182ede128081813e6
elogrus <elogrus@gmail.com>
2023-03-27 22:07:12 +0500

    fix game bug #14

--- dialogs/game/config.py
+++ dialogs/game/config.py
@@ -53,45 +53,41 @@ def createStartInfo(history):
 
 
 def getConfig(event):
-    try:
-        history = globalStorage["history"]
-        statsEnds = globalStorage["statsEnds"]
+    history = globalStorage["history"]
+    statsEnds = globalStorage["statsEnds"]
 
-        userId = getUserId(event)
-        if "game_" + userId in globalStorage:
-            info = globalStorage["game_" + userId]
-            # result['user_state_update']['lastEpisode']
+    userId = getUserId(event)
+    if "game_" + userId in globalStorage:
+        info = globalStorage["game_" + userId]
+        # result['user_state_update']['lastEpisode']
+    else:
+        cur = globalStorage["mariaDBcur"]
+        info = selectGameInfo(cur, userId)
+        if not info:
+            info = createStartInfo(history)
+
+    lastEpisode = json.loads(getGlobalState(event, 'lastEpisode'))
+    canLastChoicedArr = lastEpisode['buttons']
+    command = getCommand(event)
+
+    print('canLastChoicedArr:', canLastChoicedArr)
+
+    if canLastChoicedArr and type(canLastChoicedArr) == list and len(canLastChoicedArr) >= 2:
+        if canLastChoicedArr[0] == command:
+            print('true')
+            info["choice"] = 'true'
+        elif canLastChoicedArr[1] == command:
+            print('false')
+            info["choice"] = 'false'
         else:
-            cur = globalStorage["mariaDBcur"]
-            info = selectGameInfo(cur, userId)
-            if not info:
-                info = createStartInfo(history)
+            print('lastEP')
+            return compileResultFromEpisode(lastEpisode)
 
-        lastEpisode = json.loads(getGlobalState(event, 'lastEpisode'))
-        canLastChoicedArr = lastEpisode['buttons']
-        command = getCommand(event)
+    episode = passEpisode(info, history, statsEnds)
+    
+    print('info before', info)
+    setInGlobalStorage("game_" + userId, info, True)
+    print('info after', info)
 
-        print('canLastChoicedArr:', canLastChoicedArr)
-
-        if canLastChoicedArr and type(canLastChoicedArr) == list and len(canLastChoicedArr) >= 2:
-            if canLastChoicedArr[0] == command:
-                print('true')
-                info["choice"] = 'true'
-            elif canLastChoicedArr[1] == command:
-                print('false')
-                info["choice"] = 'false'
-            else:
-                print('lastEP')
-                compileResultFromEpisode(lastEpisode)
-
-        episode = passEpisode(info, history, statsEnds)
-        
-        print('info before', info)
-        setInGlobalStorage("game_" + userId, info, True)
-        print('info after', info)
-
-        
-        return compileResultFromEpisode(episode)
-
-    except:
-        print('Эпизод:', episode)
+    
+    return compileResultFromEpisode(episode)
