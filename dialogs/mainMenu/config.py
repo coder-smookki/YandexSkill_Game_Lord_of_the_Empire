adb512b18ce4eaf1bf746aac1992e3f2d7a73c40
frxchii <fr3chi@gmail.com>
2023-03-27 23:40:45 +0500

    fix buttons №2

--- dialogs/mainMenu/config.py
+++ dialogs/mainMenu/config.py
@@ -15,7 +15,7 @@ def getConfig(event):
         "card": {
             "type": "BigImage",
             "image_id": "1540737/f7f920f27d7c294e189b",
-            "title": "КАТАЛОГ",
+            "title": "ГЛАВНОЕ МЕНЮ",
             "description":
                 """
 Выберите нужную категорию, которая поможет вам.
