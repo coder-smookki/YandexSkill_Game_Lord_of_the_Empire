6d01ff6b6a75c84532af8e65b3ef3fd5d9328090
elogrus <elogrus@gmail.com>
2023-03-29 17:12:21 +0500

    add resetGame dialog

---
+++ dialogs/resetGame/config.py
@@ -0,0 +1,30 @@
+config = {
+        "tts":
+            """
+            Вы уверены в том, что хотите сбросить сохранение?
+            """,
+        "buttons": [
+            "Да",
+            "Нет",
+        ],
+        "card": {
+            "type": "BigImage",
+            "image_id": "1533899/d371aab5224c91137cfc",
+            "title": "СБРОСИТЬ СОХРАНЕНИЕ?",
+            "description":
+                """
+                Вы уверены в том, что хотите сбросить сохранение?
+                """,
+        },
+}
+
+session_state = {"branch": "resetGame"}
+
+
+def getConfig(event):
+    return {
+        "tts": config["tts"],
+        "buttons": config["buttons"],
+        "card": config["card"],
+        "session_state": session_state,
+    }
--- dialogs/resetGame/resetGame.py
+++ dialogs/resetGame/resetGame.py
@@ -1,14 +1,44 @@
 from utils.triggerHelper import *
 from utils.branchHandler import *
+from utils.responseHelper import *
+from .config import *
+from utils.globalStorage import *
 
 def getResponse(event, allDialogs=None):
-    response = getDialogResponseFromEnd(event, 2, allDialogs)
-    response["user_state_update"] = {'lastEpisode': None}
-    return response
+    if isInContext(event, "resetGame") and (
+        "да" in getCommand(event)
+        or "конечно" in getCommand(event)
+        or "уверен" in getCommand(event)
+        or "точно" in getCommand(event)
+        or "выйти" in getCommand(event)
+        or "выход" in getCommand(event)
+        or "выйди" in getCommand(event)
+    ):
+        userId = getUserId(event)
+        removeFromGlobalStorage("game_" + userId)
+        response = createResponse(event, getConfig(event))
+        setGlobalStatesInResponse(response, {'lastEpisode': None})
+        return response
+
+    return getDialogResponseFromEnd(event, 2, allDialogs)
 
 
 def isTriggered(event):
-    return not isInLastContext(event, 'game') and isInCommandOr(event, ["сброс"])
-
+    return (
+        (
+            isInContext(event, "exitConfirm")
+            and (
+                "да" in getCommand(event)
+                or "конечно" in getCommand(event)
+                or "уверен" in getCommand(event)
+                or "точно" in getCommand(event)
+                or "сброс" in getCommand(event)
+                or "удали" in getCommand(event)
+            )
+        )
+        or "сброс" in getCommand(event)
+        or "сброс" in getCommand(event)
+        or "сброс" in getCommand(event)
+    )
 
 backDialog = {"getResponse": getResponse, "isTriggered": isTriggered}
