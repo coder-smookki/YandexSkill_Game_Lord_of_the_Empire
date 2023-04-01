a8e0057c46ccc7b472dff0529da650a19d727ac0
elogrus <elogrus@gmail.com>
2023-04-01 18:44:03 +0500

    test stats

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from utils.dbHandler import *
 
 conn = connect('root','root', 'Lord_of_the_Empire')
 
-insertNewStat(conn, 'ggggg')
+insertNewStat(conn, 'gggggg')
 
 print(type(getStat(conn,'gggggg')))
 
