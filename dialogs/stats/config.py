a03f3797c2890614779169a832e6ae46155e7c36
elogrus <elogrus@gmail.com>
2023-04-01 16:40:36 +0500

    test stats

---
+++ dialogs/stats/config.py
@@ -0,0 +1,40 @@
+from utils.dbHandler import getStat
+from utils.globalStorage import globalStorage
+from utils.responseHelper import getUserId
+
+def getConfig(event):
+    config = {
+        "tts": """Ваша статистика:""",
+        "buttons": [
+            "Повторить ещё раз",
+            "Помощь",
+            "Назад",
+            "Выход",
+        ],
+        "card": {
+            "type": "BigImage",
+            "image_id": "1533899/d371aab5224c91137cfc",
+            "title": "СТАТИСТИКА",
+            "description": """
+            Ваша статистика:
+            """,
+        },
+    }
+
+    session_state = {"branch": "stats"}
+
+    conn = globalStorage['mariaDBconn']
+    stats = getStat(conn, getUserId(event))
+
+    config['tts'] += '\nСмерти: ' + stats['deaths']
+    config['tts'] += '\nОткрыто концовок: ' + stats['openEnds'] + ' из X'
+    config['tts'] += '\nВстречено героев: ' + stats['meetedCharacters'] + ' из 21'
+    # deaths, openEnds, meetedCharacters
+
+    return {
+        'message': config["tts"],
+        "tts": config["tts"],
+        "buttons": config["buttons"],
+        "card": config["card"],
+        "session_state": session_state,
+    }
