7cdc43d89a64e3cf63f3303c623c2812d58bdb45
elogrus <elogrus@gmail.com>
2023-04-01 15:26:03 +0500

    test stats

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from utils.dbHandler import *
 conn = connect('root','root', 'Lord_of_the_Empire')
 
 print(getStat(conn, 'someUserId', 'deaths'))
-reduceStat(conn,'someUserId', deaths=1)
+setStat(conn,'someUserId', deaths=15)
 print(getStat(conn, 'someUserId', 'deaths'))
