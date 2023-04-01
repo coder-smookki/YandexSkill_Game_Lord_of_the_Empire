04f53cf93811372e3999d998685fc38d7e222951
elogrus <elogrus@gmail.com>
2023-04-01 17:49:53 +0500

    test stats

--- dialogs/stats/config.py
+++ dialogs/stats/config.py
@@ -38,12 +38,12 @@ def getConfig(event):
         } 
 
     config['tts'] += '\nСмерти: ' + str(stats['deaths'])
-    config['tts'] += '\nОткрыто концовок: ' + str(stats['openEnds']) + ' из X'
-    config['tts'] += '\nВстречено героев: ' + str(stats['meetedCharacters']) + ' из 21'
+    config['tts'] += '\nОткрыто концовок: ' + str(len(stats['openEnds'])) + ' из X'
+    config['tts'] += '\nВстречено героев: ' + str(len(stats['meetedCharacters'])) + ' из 21'
 
     config['card']['description'] += '\nСмерти: ' + str(stats['deaths'])
-    config['card']['description'] += '\nОткрыто концовок: ' + str(stats['openEnds']) + ' из X'
-    config['card']['description'] += '\nВстречено героев: ' + str(stats['meetedCharacters']) + ' из 21'
+    config['card']['description'] += '\nОткрыто концовок: ' + str(len(stats['openEnds'])) + ' из X'
+    config['card']['description'] += '\nВстречено героев: ' + str(len(stats['meetedCharacters'])) + ' из 21'
 
     # deaths, openEnds, meetedCharacters
 
