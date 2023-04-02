8a479391db575bbf54afd9ea8315384bda33cbcd
elogrus <elogrus@gmail.com>
2023-04-03 03:53:25 +0500

    last

--- dialogs/mainMenu/mainMenu.py
+++ dialogs/mainMenu/mainMenu.py
@@ -13,6 +13,7 @@ def getResponse(event, allDialogs=None):
     if haveGlobalState(event, 'addStats') and type(getGlobalState(event, 'addStats')) == list:
         stats = getGlobalState(event, 'addStats')
         for stat in stats:
+            print('mainMenuStat')
             increaseStat(conn, getUserId(event), deaths=stat[0], openEnds=stat[1])
         if not 'user_state_update' in config:
             config['user_state_update'] = {}
