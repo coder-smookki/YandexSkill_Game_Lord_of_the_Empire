7eddb2691ce8d1b5ab238fe9df022b52668b7b53
elogrus <elogrus@gmail.com>
2023-04-02 22:42:53 +0500

    last

--- dialogs/howToUse/config.py
+++ dialogs/howToUse/config.py
@@ -34,7 +34,7 @@ def getConfig(event):
         "card": {
             "type": "BigImage",
             "image_id": "1521359/3349afd66fa90845fd03",
-            "title": "КАК ПОЛЬЗОВАТЬСЯ?",
+            "title": "КАК ИГРАТЬ?",
             "description": tts,
         },
     }
