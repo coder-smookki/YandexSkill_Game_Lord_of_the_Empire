6986aa01c8818894b3169fbb750a220281aaf152
elogrus <elogrus@gmail.com>
2023-04-03 21:20:13 +0500

    fix

--- dialogs/exitConfirm/exitConfirm.py
+++ dialogs/exitConfirm/exitConfirm.py
@@ -1,4 +1,4 @@
-from utils.intents import ExitIntents, YesIntents
+from utils.intents import ExitIntents, YesIntents, NoIntents
 from .config import *
 from utils.triggerHelper import *
 from utils.responseHelper import *
@@ -8,11 +8,11 @@ from utils.branchHandler import getDialogResponseFromEnd
 def getResponse(event, allDialogs=None):
     isYes = isInCommandOr(event, YesIntents)
     isExit = isInCommandOr(event, ExitIntents)
-
+    isNo = isInCommandOr(event, NoIntents)
     print(isYes)
     print(isExit)
     if isInLastContext(event, 'exitConfirm'):
-        if isExit or isYes:
+        if isExit or isYes and not isNo:
             return getConfirmResponse(event)
         return getDialogResponseFromEnd(event, 2, allDialogs)
 
