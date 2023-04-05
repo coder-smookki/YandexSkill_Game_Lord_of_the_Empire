4a11f8be2a1bef258a486a04047210be730327e1
elogrus <elogrus@gmail.com>
2023-04-05 13:51:32 +0500

    fix

--- dialogs/game/config.py
+++ dialogs/game/config.py
@@ -466,7 +466,7 @@ def getConfig(event, allDialogs, needCreateNewInfo=False, fromGame=True, repeat=
         return mainMenuGetConfig(event)
 
     # вернуть прошлый эпизод, если игрок попросил повторить
-    if isInCommandOr(event, RepeatIntents) and (canLastChoicedArr and len(canLastChoicedArr) != 0) or not fromGame:
+    if isInCommandOr(event, RepeatIntents) and (canLastChoicedArr and len(canLastChoicedArr) != 0):
         config = compileConfigFromEpisode(event, lastEpisode, haveUserInterface)
         canLastState = {'canLastChoiced': canLastChoicedArr}
         if not "user_state_update" in config:
