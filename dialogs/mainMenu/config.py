640c6cbb7385fd3eff017fd29f6ded2f84a0ab44
elogrus <elogrus@gmail.com>
2023-04-05 13:59:06 +0500

    fix

--- dialogs/mainMenu/config.py
+++ dialogs/mainMenu/config.py
@@ -1,3 +1,8 @@
+from utils.triggerHelper import *
+from utils.responseHelper import *
+from utils.dbHandler import *
+from utils.globalStorage import *
+
 def getConfig(event):
     config = {
         "tts":
@@ -33,10 +38,24 @@ def getConfig(event):
 
     session_state = {"branch": "mainMenu"}
 
-    return {
+    config = {
         'message': config["tts"],
         "tts": config["tts"],
         "buttons": config["buttons"],
         "card": config["card"],
         "session_state": session_state,
     }
+
+    conn = globalStorage["mariaDBconn"]
+
+    config['user_state_update'] = {}
+    config['user_state_update']['addStats'] = []
+
+    if haveGlobalState(event, 'addStats') and type(getGlobalState(event, 'addStats')) == list:
+        stats = getGlobalState(event, 'addStats')
+        for stat in stats:
+            print('mainMenuStat')
+            increaseStat(conn, getUserId(event), deaths=stat[0], openEnds=stat[1])
+        if not 'user_state_update' in config:
+            config['user_state_update'] = {}
+        config['user_state_update']['addStats'] = []
